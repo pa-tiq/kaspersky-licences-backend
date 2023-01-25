a14f0606cdb3f416803a52373e2cb39772abd298
pa-tiq <patrickdsilva99@gmail.com>
2023-01-25 08:57:42 -0400

    refactor

--- controllers/licences_controller.js
+++ controllers/licences_controller.js
@@ -33,34 +33,4 @@ exports.getLicences = (req, res, next) => {
     );
   });
 
-  // connect to your database
-  //sql.connect(config, function (err) {
-  //  if (err) console.log(err);
-  //
-  //  // create Request object
-  //  const request = new sql.Request();
-  //
-  //  // query to the database and get the records
-  //  request.query(
-  //    `
-  //    SELECT
-  //    G.wstrName as 'OM',
-  //    count(G.wstrName)  as 'qtd licencas'
-  //    FROM dbo.apphostskeys AHK
-  //    INNER JOIN dbo.v_hosts H ON AHK.nIdHost=H.nId
-  //    INNER JOIN dbo.v_adm_group G ON H.nGroup = G.nId
-  //    group by G.wstrName
-  //    `,
-  //    function (err, recordset) {
-  //      if (err) console.log(err);
-  //      // send records as a response
-  //      res.send(recordset);
-  //    }
-  //  );
-  //});
-
-  //res.status(200).json({
-  //  // 200 = success
-  //  posts: [{ title: 'oieeeee', content: 'diaaaaaaaaaaaaaaaaaaaaaaaaaaa' }],
-  //});
 };
