f8a2a5ed727f61e310ed31056a2c74a2aa110a52
pa-tiq <patrickdsilva99@gmail.com>
2023-03-03 10:48:48 -0400

    both servers

--- controllers/licences_controller.js
+++ controllers/licences_controller.js
@@ -1,13 +1,23 @@
 const sql = require('mssql/msnodesqlv8');
 const keys = require('../keys.json');
 
-exports.getLicences = (req, res, next) => {
+const licencesQuery = `
+  SELECT
+  G.wstrName as 'OM',
+  count(G.wstrName)  as 'qtd_licencas' 
+  FROM dbo.apphostskeys AHK 
+  INNER JOIN dbo.v_hosts H ON AHK.nIdHost=H.nId
+  INNER JOIN dbo.v_adm_group G ON H.nGroup = G.nId
+  group by G.wstrName
+  `;
+
+exports.getLicences183 = (req, res, next) => {
   // config for your database
   let result_object;
   const config = {
     user: keys.mssql_username,
-    password: keys.mssql_password184,
-    server: 'localhost',
+    password: keys.mssql_password183,
+    server: keys.mssql_server183,
     driver: 'msnodesqlv8',
     port: keys.mssql_port,
     database: keys.mssql_database,
@@ -19,21 +29,39 @@ exports.getLicences = (req, res, next) => {
   try {
     const pool = new sql.ConnectionPool(config);
     pool.connect().then(() => {
-      pool.request().query(
-        `
-        SELECT
-        G.wstrName as 'OM',
-        count(G.wstrName)  as 'qtd_licencas' 
-        FROM dbo.apphostskeys AHK 
-        INNER JOIN dbo.v_hosts H ON AHK.nIdHost=H.nId
-        INNER JOIN dbo.v_adm_group G ON H.nGroup = G.nId
-        group by G.wstrName
-        `,
-        (err, result) => {
-          result_object = result;
-          res.send(result);
-        }
-      );
+      pool.request().query(licencesQuery, (err, result) => {
+        result_object = result;
+        res.send(result);
+      });
+    });
+  } catch (error) {
+    console.log(error);
+    res.send(result_object);
+  }
+};
+
+exports.getLicences184 = (req, res, next) => {
+  // config for your database
+  let result_object;
+  const config = {
+    user: keys.mssql_username,
+    password: keys.mssql_password184,
+    server: keys.mssql_server184,
+    driver: 'msnodesqlv8',
+    port: keys.mssql_port,
+    database: keys.mssql_database,
+    options: {
+      trustedConnection: true,
+    },
+  };
+
+  try {
+    const pool = new sql.ConnectionPool(config);
+    pool.connect().then(() => {
+      pool.request().query(licencesQuery, (err, result) => {
+        result_object = result;
+        res.send(result);
+      });
     });
   } catch (error) {
     console.log(error);
--- routes/licences_router.js
+++ routes/licences_router.js
@@ -2,6 +2,7 @@ const express = require("express");
 const licencesController = require("../controllers/licences_controller");
 const router = express.Router();
 
-router.get("/", licencesController.getLicences); // GET /licences/
+router.get("/183", licencesController.getLicences183); 
+router.get("/184", licencesController.getLicences184); 
 
 module.exports = router;
