37b4964c85b9f07a6a6aa4f84fce661ca103794d
pa-tiq <patrickdsilva99@gmail.com>
2023-03-06 14:10:41 -0400

    send only if no error

--- controllers/licences_controller.js
+++ controllers/licences_controller.js
@@ -1,28 +1,27 @@
 const sql = require('mssql');
 const keys = require('../keys.json');
 
-const licencesQuery = 
-` SELECT
+const licencesQuery = ` SELECT
   G.wstrName as 'OM',
   count(G.wstrName)  as 'qtd_licencas' 
   FROM dbo.apphostskeys AHK 
   INNER JOIN dbo.v_hosts H ON AHK.nIdHost=H.nId
   INNER JOIN dbo.v_adm_group G ON H.nGroup = G.nId
   group by G.wstrName
-  `
+  `;
 const configCommon = {
   database: keys.mssql_database,
   user: keys.mssql_username,
   pool: {
     max: 10,
     min: 0,
-    idleTimeoutMillis: 30000
+    idleTimeoutMillis: 30000,
   },
   options: {
     trustedConnection: true,
-    trustServerCertificate: true
+    trustServerCertificate: true,
   },
-}
+};
 
 exports.getLicences183 = (req, res, next) => {
   let result_object_183 = {};
@@ -30,57 +29,54 @@ exports.getLicences183 = (req, res, next) => {
     password: keys.mssql_password183,
     server: keys.mssql_server183,
     port: keys.mssql_port183,
-    ...configCommon
+    ...configCommon,
   };
 
   const sendQuery = async () => {
     try {
       await sql.connect(config);
-      new sql.Request().query(licencesQuery,(err,result) => {
-        if(err){
+      new sql.Request().query(licencesQuery, (err, result) => {
+        if (err) {
           throw new Error(err.message);
-        }
-        else{
+        } else {
           result_object_183 = result.recordset;
           res.send(result.recordset);
         }
-      })
-     } catch (err) {
+      });
+    } catch (err) {
       console.log(err);
-      res.send(result_object_183);
-     }
-  }
+      res.send({ result_object_183, err });
+    }
+  };
 
   sendQuery();
 };
 
 exports.getLicences184 = (req, res, next) => {
-
   let result_object_184 = {};
   const config = {
     password: keys.mssql_password184,
     server: keys.mssql_server184,
     port: keys.mssql_port184,
-    ...configCommon
+    ...configCommon,
   };
 
   const sendQuery = async () => {
     try {
       await sql.connect(config);
-      new sql.Request().query(licencesQuery,(err,result) => {
-        if(err){
+      new sql.Request().query(licencesQuery, (err, result) => {
+        if (err) {
           throw new Error(err.message);
-        }
-        else{
+        } else {
           result_object_184 = result.recordset;
           res.send(result.recordset);
         }
-      })
-     } catch (err) {
+      });
+    } catch (err) {
       console.log(err);
-      res.send(result_object_184);
-     }
-  }
+      res.send({ result_object_184, err });
+    }
+  };
 
   sendQuery();
 };
