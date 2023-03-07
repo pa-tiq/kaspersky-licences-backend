f7513aae07a06a8575a65b0feb26ff3a38e87801
pa-tiq <patrickdsilva99@gmail.com>
2023-03-07 09:13:49 -0400

    bug fix

--- controllers/licences_controller.js
+++ controllers/licences_controller.js
@@ -24,11 +24,13 @@ const tryGetLicenceCount = (res,config, server) => {
   sql
     .connect(config)
     .then(() => {
+      console.log(`Buscando licenças no ${server}...`);
       return new sql.Request().query(licencesQuery);
     })
     .then((err, result) => {
       if (err) {
         if (err.recordset) {
+          console.log(`Licenças do ${server} obtidas!`);
           let data = JSON.stringify(err.recordset);
           fs.writeFileSync(`${server}.json`, data);
           res.send(err.recordset);
@@ -37,6 +39,7 @@ const tryGetLicenceCount = (res,config, server) => {
           throw new Error(err.message);
         }
       } else {
+        console.log(`Licenças do ${server} obtidas!`);
         let data = JSON.stringify(result.recordset);
         fs.writeFileSync(`${server}.json`, data);
         res.send(result.recordset);
@@ -48,29 +51,30 @@ const tryGetLicenceCount = (res,config, server) => {
       if (rawdata) {
         data = JSON.parse(rawdata);
       }
+      console.log('Erro!')
       console.log(error);
       res.send({ data, error });
     });
 }
 
 exports.get183Licences = (req, res, next) => {
-  const config = {
+  const config183 = {
     password: keys.mssql_password183,
     server: keys.mssql_server183,
     port: keys.mssql_port183,
     ...configCommon,
   };
 
-  tryGetLicenceCount(res,config,'183');
+  tryGetLicenceCount(res,config183,'183');
 };
 
 exports.get184Licences = (req, res, next) => {
-  const config = {
+  const config184 = {
     password: keys.mssql_password184,
     server: keys.mssql_server184,
     port: keys.mssql_port184,
     ...configCommon,
   };
 
-  tryGetLicenceCount(res,config,'184');
+  tryGetLicenceCount(res,config184,'184');
 };
