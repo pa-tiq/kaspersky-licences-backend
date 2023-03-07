bc142c02f807f0ccadf4085ba661e20a0e4ef0c6
pa-tiq <patrickdsilva99@gmail.com>
2023-03-07 09:39:18 -0400

    bug fix

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ app.use(bodyParser.json()); // parse incoming JSON data
 
 app.use((req, res, next) => {
   //middleware to solve CORS error
+  res.set('Cache-control','no-store');
   res.setHeader("Access-Control-Allow-Origin", "*"); //allow origins to access my data
   res.setHeader("Access-Control-Allow-Methods", "GET"); //allow origins to use my HTTP methods
   res.setHeader("Access-Control-Allow-Headers", "Content-type, Authorization"); //allow origins to use certain headers
