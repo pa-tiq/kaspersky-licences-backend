4170df6d1de9d8fe71f273890d2400ae9e33f46a
pa-tiq <patrickdsilva99@gmail.com>
2023-03-07 09:55:03 -0400

    disable cached response

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ app.use(bodyParser.json()); // parse incoming JSON data
 
 app.use((req, res, next) => {
   //middleware to solve CORS error
-  res.setHeader('Cache-control','no-store');
+  res.setHeader('Cache-control','no-cache, private, no-store, must-revalidate, max-stale=0, post-check=0, pre-check=0');
   res.setHeader("Access-Control-Allow-Origin", "*"); //allow origins to access my data
   res.setHeader("Access-Control-Allow-Methods", "GET"); //allow origins to use my HTTP methods
   res.setHeader("Access-Control-Allow-Headers", "Content-type, Authorization"); //allow origins to use certain headers
@@ -15,16 +15,5 @@ app.use((req, res, next) => {
 });
 
 app.use("/licences", licences_router); 
-
-app.use((error, req, res, next) => {
-  // executed whenever an error is thrown with throw() or forwarded with next()
-  console.log('Erro capturado:', error);
-  const status = error.statusCode || 500; // if error.statusCode is undefined, then status = 500
-  const message = error.message;
-  const data = error.data;
-  //if (!message.includes('invalid signature')){
-  res.status(status).json({ message: message, data: data });
-  //}
-});
 
 app.listen(8080);
